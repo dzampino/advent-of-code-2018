8b461e0f1f05e2b053969f5572a6157bfda355ad
David Zampino <dzampino@users.noreply.github.com>
2018-12-05 15:14:05 -0600

    Complete part one

--- day1.ps1
+++ day1.ps1
@@ -1,1 +1,11 @@
-$freq = 0
+[int]$frequency = 0
+
+foreach ($line in (Get-Content .\day1.input)) {
+    if ($line[0] -eq "+") {
+        $frequency = $frequency + [int]$line.trim("+")
+    }
+    else {
+        $frequency = $frequency + [int]$line
+    }
+}
+$frequency
